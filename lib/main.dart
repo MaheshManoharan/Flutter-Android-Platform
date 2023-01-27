c0e11364ec1ff4fb8de8a4dd53c8e99ed62fbe63
mahesh <maheshmanoharan7251@gmail.com>
2023-01-27 10:48:02 +0530

    files added

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> 
+{
+
+  String batteryLevel = 'Waiting...';
+  static const batteryChannel = MethodChannel('com.mahi/battery');
+
+    Future getBatteryLevel() async
+    {
+     final int newBatteryLevel = await batteryChannel.invokeMethod('getBatteryLevel');
+
+     setState(() {
+       batteryLevel = '$newBatteryLevel';
+     });
+    }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Column(children: [
+          Text(batteryLevel),
+          ElevatedButton(
+            child: Text("get Battery level"),
+            onPressed: getBatteryLevel,
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+
+
+// String batteryLevel = 'Waiting...';
+//   static const batteryChannel = MethodChannel('mahidev.com/battery');
+//   Future getBatteryLevel() async 
+//   {
+//   //  final arguments = {'name': 'Sarah Abs'};
+//      String newBatteryLevel =
+//         await batteryChannel.invokeMethod('getBatteryLevel');//, arguments);
+//     setState(() => batteryLevel = newBatteryLevel);
+//   }
